6e3038cb274313b3265151e8bae2ba1ecca99bac
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2024-09-16 08:13:55 -0700

    Backend Stuff

--- Backend/week02/expressGamingSolution/routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", ((req, res) =>{
-    res.json({message : "Welcome to Games info App"})
-}))
-
-
-module.exports = router;
--- Backend/week03/IAMongodSolutionRoutesFolder/model/Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const productSchema = new mongoose.Schema({
-    productName : {
-        type: String
-    },
-})
-
-module.exports = mongoose.model("product", productSchema);
